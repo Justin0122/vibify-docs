12ccc136d91d93216c7c410b00719843f95987e0
Justin Jongstra <00300061@rijnijssel.nl>
2024-04-25 10:30:30 +0200

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://vibify-docs.justinjongstra.nl',
 	integrations: [
 		starlight({
 			title: 'Vibify',
